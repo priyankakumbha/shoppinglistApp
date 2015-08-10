04e0805f8329c96992016c093db008c2f355e61c
priyankakumbha <priyankakumbhar789@gmail.com>
2015-08-10 21:18:25 +1000

    added validation

--- js/app.js
+++ js/app.js
@@ -1,25 +1,31 @@
 $(document).ready(function(){
-   if ($("#item").is(':empty')){
-      console.log("list is empty");
-    $("#button").hide();
-}
 $( "#addItems" ).keypress(function(e) {
   if (e.keyCode == 13) {
   var itemvalue = $("#addItems" ).val();
-  
-  addItemToList(itemvalue);
-  $("#addItems").val('');
- $("#button").show();
+  if(itemvalue!= null && itemvalue!=""){
+    $("#errorMessage").text(""); 
+    //Do not add item if already exist
+    if (! $('#items li:contains("' + itemvalue + '")').length) {
+    addItemToList(itemvalue);
+    $("#addItems").val('');
+    $("#button").show();
+    }
+    else{
+      $("#errorMessage").html("<h3> Item already exist</h3>");
+      $("#addItems").val('');
+    }
+  }
+
   }
 
 });
 
-
-
-$(document).on("click", ".delete", function(){
-    
-    $(this).closest('#item').fadeOut(300);
-
+$(document).on("click", ".delete", function(){    
+    $(this).closest('#item').fadeOut(300); 
+    $(this).closest('#item').remove();
+    if($("#item").children().length==0){
+    $("#button").hide();
+    }
 });
 
 $(document).on("click", "#button", function(){
@@ -27,18 +33,17 @@ $(document).on("click", "#button", function(){
     $("#items").empty();
     $("#button").hide();
 });
-$(document).on("click", ".right", function(){
-    
+
+$(document).on("click", ".right", function(){    
        
-        $(this).closest('#item').addClass("strikethrough");
+  $(this).closest('#item').addClass("strikethrough");
 
 });
 
 });
 function addItemToList(itemvalue) {
 	var checkMark = '<span class="right"></span> ';
-    var xMark = '<span class="delete"></span>';
-    $( "#items" ).prepend( $( "<div id='space'> <li id='item'> "+checkMark+itemvalue+ xMark+" </li> </div>") );
-   
+  var xMark = '<span class="delete"></span>';
+  $( "#items" ).prepend( $( "<div id='space'> <li id='item'> "+checkMark+itemvalue+ xMark+" </li> </div>") );
        
-    }
+  }
